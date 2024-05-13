8746c19c88b1f8bb2a934f4d6283a2dfce917305
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-05-13 16:08:37 +0200

    Refactored client code to oop

--- client.py
+++ client.py
@@ -1,70 +1,73 @@
 import json
 import socket
-from message import Message
+from os import system, name
 
 
-# class Client:
-#     def __init__(self, host, port, client_sock=None):
-#         self.host = host
-#         self.port = port
-#         if client_sock is None:
-#             self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         else:
-#             self.client_sock = client_sock
-#         self.msg = None
-#
-#     def connect(self):
-#         self.client_sock.connect((self.host, self.port))
-#         print("Connected to server!")
-#
-#     def send_message(self, msg):
-#         self.client_sock.sendall(bytes(json.dumps(msg), encoding="utf-8"))
-#
-#     def receive_message(self):
-#         self.msg = (json.loads(self.client_sock.recv(1024).decode("utf-8")))
-#
-#     def print_to_terminal(self):
-#         for key, value in self.msg.items():
-#             print(f"\n{key}: {value}")
-#
-#
-# client = Client("127.0.0.1", 65000)
-# client.connect()
+class Client:
+    def __init__(self, host, port, client_sock=None):
+        self.host = host
+        self.port = port
+        if client_sock is None:
+            self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.client_sock = client_sock
 
-host = "127.0.0.1"
-port = 65000
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_sock.connect((host, port))
-msg = client_sock.recv(1024).decode("utf-8")
-decoded_msg = (json.loads(msg))
-print(decoded_msg)
+    def connect(self):
+        self.client_sock.connect((self.host, self.port))
 
-valid_requests = ["info", "help", "uptime", "close"]
-while True:
-    client_request = input("Enter command: ")
-    client_msg = Message(client_request)
-    while True:
+    def send(self, msg):
+        self.client_sock.send(bytes(json.dumps(msg), "utf-8"))
+
+    def receive(self):
+        message = json.loads(self.client_sock.recv(1024).decode("utf-8"))
+        return message
+
+    @staticmethod
+    def print_to_terminal(received_msg):
         try:
-            if client_msg.msg_text in valid_requests[:2]:
-                client_sock.send(bytes(client_msg.write(), "utf-8"))
-                server_response = (json.loads(client_sock.recv(1024).decode("utf-8")))
-                for key, value in server_response.items():
-                    print(f"{key} - {value}")
-                break
-            elif client_request in valid_requests[-1]:
-                client_sock.send(bytes(json.dumps(client_request), "utf-8"))
-                server_response = (json.loads(client_sock.recv(1024).decode("utf-8")))
-                print(server_response)
-                # client_sock.shutdown(socket.SHUT_RDWR)
-                # client_sock.close()
-                exit()
+            if isinstance(received_msg, dict):
+                for key, value in received_msg.items():
+                    print(f"{key}: {value}")
+                print()
+            elif isinstance(received_msg, str):
+                print(received_msg)
+                print()
             else:
                 raise ValueError
         except ValueError:
-            print("Invalid request. Choose 'info', 'help', 'uptime', 'close' ")
-            break
+            print("Invalid message format!")
+            exit()
 
-# decode and print commands, info, uptime
+    @staticmethod
+    def clr_screen():
+        if name == 'nt':
+            _ = system('cls')
+        else:
+            _ = system('clear')
+
+    def run(self):
+        self.connect()
+        while True:
+            try:
+                server_response = self.receive()
+                if server_response == "Invalid request":
+                    raise ValueError
+                else:
+                    self.print_to_terminal(server_response)
+                    request = input("Enter command: ")
+                    self.send(request)
+                    self.clr_screen()
+                    if request == "close":
+                        self.client_sock.shutdown(socket.SHUT_RDWR)
+                        self.client_sock.close()
+                        break
+            except ValueError:
+                print("Invalid request!")
 
 
+if __name__ == "__main__":
+    client = Client("127.0.0.1", 65000)
+    client.run()
 
+# server response + cls screen
+# read multiple messages
