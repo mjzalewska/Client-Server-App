845d70666709229e499e520e44f5fd436fdb4a91
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-05-22 23:46:35 +0200

    Added a separate user class

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, login, passphrase, role):
+        self.login = login
+        self.passphrase = passphrase
+        self.role = role
+        self.logged_in = False
+        self.inbox = {}
+
+
+
+
+
