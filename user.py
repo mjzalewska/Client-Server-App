e1c1c80ce4d37f90c97dbad32341cd6a3a73a395
magdazalewska <m.jarzebska.zalewska@gmail.com>
2024-05-31 22:59:23 +0200

    Added User class methods (add, remove, log in, log out)

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+from db_manager import DbManager
+
+
 class User:
     def __init__(self, login, passphrase, role):
         self.login = login
@@ -6,7 +9,36 @@ class User:
         self.logged_in = False
         self.inbox = {}
 
+    @staticmethod
+    def does_user_exist(user_name):
+        if DbManager.fetch(user_name):
+            return True
+        else:
+            return False
 
+    def log_in(self, user):
+        user_name, password = user
+        if self.does_user_exist(user_name):
+            if DbManager.fetch(user_name)["password"] == password:
+                self.logged_in = True
 
+    def log_out(self):
+        self.logged_in = False
 
+    @staticmethod
+    def add(user):
+        user_name, password = user
+        if DbManager.fetch(user_name) is None:
+            DbManager.add({user_name: {"password": password, "role": "user"}})
+            return True
+        else:
+            return False
 
+    @staticmethod
+    def remove(user):
+        user_name = user[0]
+        if DbManager.fetch(user_name):
+            DbManager.remove(user_name)
+            return True
+        else:
+            return False
