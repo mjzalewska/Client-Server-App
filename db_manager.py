14c5898abe01adf67c8ec4a46c53c3870f345a24
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-07-14 08:06:14 +0200

    Updated remove and fetch methods

--- db_manager.py
+++ db_manager.py
@@ -46,34 +46,53 @@ class DbManager:
 
         with open(db_name, 'w') as new_db_file:
             json.dump(existing_data, new_db_file, indent=4)
-            return {"message": f"updated {updated_recs} record"}
+            return {"message": f"updated {updated_recs} record(s)"}
 
 
     @classmethod
     def remove(cls, db_name, user_name):
-        updated_records = []
-        with open(db_name, "r+", encoding="utf-8") as db:
-            cache = [eval(line.rstrip(",")) for line in db.read().splitlines()]
-            for record in cache:
-                if record["username"] != user_name:
-                    updated_records.append(record)
-                else:
-                    pass
-            db.truncate(0)
-            for record in updated_records:
-                cls.add(db_name, record, "a+")
+        try:
+            if os.path.exists(db_name):
+                updated_data = []
+                with open(db_name, "r", encoding="utf-8") as old_db_file:
+                    try:
+                        existing_data = json.load(old_db_file)
+                        removed_recs = 0
+                        for record in existing_data:
+                            if record["username"] != user_name:
+                                updated_data.append(record)
+                            else:
+                                removed_recs += 1
+                    except json.JSONDecodeError:
+                        updated_data = []
+            else:
+                raise FileNotFoundError
+        except FileNotFoundError:
+            return {"message": "database not found"}
+        except Exception as e:
+            return {"message": e}
+
+        with open(db_name, 'w') as new_db_file:
+            json.dump(updated_data, new_db_file, indent=4)
+            return {"message": f"updated {removed_recs} record(s)"}
 
     @classmethod
     def fetch(cls, db_name, user_name):
-        with open(db_name, "r+") as db:
-            cache = [eval(line.rstrip(",")) for line in db.read().splitlines()]
-            return [record for record in cache if record["username"] == user_name]
+        try:
+            if os.path.exists(db_name):
+                with open(db_name, "r", encoding="utf-8") as old_db_file:
+                    try:
+                        existing_data = json.load(old_db_file)
+                        return [record for record in existing_data if record["username"] == user_name]
+                    except json.JSONDecodeError:
+                        return []
+            else:
+                raise FileNotFoundError
+        except FileNotFoundError:
+            return {"message": "database not found"}
+        except Exception as e:
+            return {"message": e}
 
 
-# DbManager.add("users.json", {"username": "johnny_boy", "password": "strongpass123", "inbox": []})
-# DbManager.add("users.json", {"username": "johnny_gal", "password": "strongpass456", "inbox": []})
-# DbManager.add("users.json", {"username": "vito_bambino", "password": "strongpass456", "inbox": []})
-# DbManager.remove("users.json", "bunny_man")
-# DbManager.create_db()
-# print(DbManager.update("users.json", "steven", "username", "mr_stevens"))
-# print(DbManager.fetch("users.json", "johnny_gal"))
+
+
