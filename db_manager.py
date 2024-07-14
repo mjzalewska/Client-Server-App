45d78595cc196bfd7573de687b71ffadfbc2d9bc
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-07-14 07:51:39 +0200

    Updated add  and update methods

--- db_manager.py
+++ db_manager.py
@@ -1,26 +1,54 @@
 import json
+import os
 
 
 class DbManager:
 
     @classmethod
-    def add(cls, db_name, data, flag):
-        with open(db_name, flag, encoding="utf-8") as db:
-            db.write(json.dumps(data))
-            db.write("\n")
+    def add(cls, db_name, data):
+        if os.path.exists(db_name):
+            with open(db_name, 'r') as db:
+                try:
+                    existing_data = json.load(db)
+                except json.JSONDecodeError:
+                    existing_data = []
+        else:
+            existing_data = []
+
+        existing_data.append(data)
+
+        with open(db_name, 'w') as db:
+            json.dump(existing_data, db, indent=4)
+
 
     @classmethod
     def update(cls, db_name, user_name, key, new_key_val):
-        with open(db_name, "r+", encoding="utf-8") as db:
-            cache = [eval(line.rstrip(",")) for line in db.read().splitlines()]
-            for record in cache:
-                if record["username"] == user_name:
-                    record[key] = new_key_val
-                else:
-                    pass
-            db.truncate(0)
-            for new_record in cache:
-                cls.add(db_name, new_record, "a+")
+        try:
+            if os.path.exists(db_name):
+                with open(db_name, "r", encoding="utf-8") as old_db_file:
+                    try:
+                        existing_data = json.load(old_db_file)
+                        updated_recs = 0
+                        for record in existing_data:
+                            if record["username"] == user_name:
+                                record[key] = new_key_val
+                                updated_recs +=1
+                            else:
+                                pass
+                    except json.JSONDecodeError:
+                        existing_data = []
+            else:
+                raise FileNotFoundError
+        except FileNotFoundError:
+            return {"message": "database not found"}
+        except Exception as e:
+            return {"message": e}
+
+        with open(db_name, 'w') as new_db_file:
+            json.dump(existing_data, new_db_file, indent=4)
+            return {"message": f"updated {updated_recs} record"}
+
+
     @classmethod
     def remove(cls, db_name, user_name):
         updated_records = []
@@ -42,11 +70,10 @@ class DbManager:
             return [record for record in cache if record["username"] == user_name]
 
 
-
-# DbManager.add("users.json", {"username": "johnny_boy", "password": "strongpass123", "inbox": []}, "a+")
-# DbManager.add("users.json", {"username": "johnny_gal", "password": "strongpass456", "inbox": []}, "a+")
-# DbManager.add("users.json", {"username": "vito_bambino", "password": "strongpass456", "inbox": []}, "a+")
+# DbManager.add("users.json", {"username": "johnny_boy", "password": "strongpass123", "inbox": []})
+# DbManager.add("users.json", {"username": "johnny_gal", "password": "strongpass456", "inbox": []})
+# DbManager.add("users.json", {"username": "vito_bambino", "password": "strongpass456", "inbox": []})
 # DbManager.remove("users.json", "bunny_man")
 # DbManager.create_db()
-# DbManager.update("users.json", "johnny_gal", "username", "bunny_man")
+# print(DbManager.update("users.json", "steven", "username", "mr_stevens"))
 # print(DbManager.fetch("users.json", "johnny_gal"))
