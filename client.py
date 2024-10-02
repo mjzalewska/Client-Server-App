78f21d0906375418a4a970cc5b2b0560243a4d89
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-10-02 15:38:41 +0200

    1)Refactored receive method to correctly receive and decode messages with headers 2)Updated the print to terminal method to account for the fact that message format validation is included in the receive method

--- client.py
+++ client.py
@@ -11,6 +11,7 @@ class Client:
             self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.client_sock = client_sock
+        self.buffer = 1024
 
     def connect(self):
         self.client_sock.connect((self.host, self.port))
@@ -19,24 +20,34 @@ class Client:
         self.client_sock.send(bytes(json.dumps(msg), "utf-8"))
 
     def receive(self):
-        message = json.loads(self.client_sock.recv(1024).decode("utf-8"))
-        return message
+        msg_parts = []
+        bytes_rec = 0
+        try:
+            header = self.client_sock.recv(4)
+            if not header:
+                raise ValueError
+            else:
+                msg_len = int.from_bytes(header[0:4], byteorder="big")
+                while bytes_rec < msg_len:
+                    msg_part = self.client_sock.recv(min(msg_len - bytes_rec, self.buffer))
+                    if not msg_part:
+                        raise RuntimeError
+                    msg_parts.append(msg_part)
+                    bytes_rec += len(msg_part)
+                data = b"".join(msg_parts)
+                message = data.decode("utf-8").strip()
+                return message
+
+        except ValueError:
+            print("Invalid message format: missing header!")
+        except RuntimeError:
+            print("Invalid message format: missing data!")
 
     @staticmethod
     def print_to_terminal(received_msg):
-        try:
-            if isinstance(received_msg, dict):
-                for key, value in received_msg.items():
-                    print(f"{key}: {value}")
-                print()
-            elif isinstance(received_msg, str):
-                print(received_msg)
-                print()
-            else:
-                raise ValueError
-        except ValueError:
-            print("Invalid message format!")
-            exit()
+        for key, value in received_msg.items():
+            print(f"{key}: {value}")
+            print()
 
     @staticmethod
     def clr_screen():
@@ -49,8 +60,9 @@ class Client:
         self.connect()
         while True:
             try:
-                server_response = self.receive()
-                self.print_to_terminal(server_response)
+                server_response = self.receive()  # +57
+                print(server_response)
+                # self.print_to_terminal(server_response)
                 request = input(">>: ")
                 self.send(request)
                 self.clr_screen()
@@ -62,6 +74,7 @@ class Client:
                 exit()
             except Exception as e:
                 print(e)
+                exit()  ## remove in final
 
 
 if __name__ == "__main__":
