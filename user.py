d6f3f94415577f2e640815b5d808fbc6c4763238
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-11-07 00:46:44 +0100

    Updated the user class attributes, added hash_password() method

--- user.py
+++ user.py
@@ -1,51 +1,68 @@
+import hashlib
+
 from db_manager import DbManager
 
 
 class User:
-    def __init__(self):
-        self.login = None
-        self.passphrase = None
-        self.role = "user"
-        self.logged_in = False
-        self.inbox = {}
+    def __init__(self, username, password_hash, email, role="user"):
+        self.username = username
+        self.password_hash = password_hash
+        self.role = role
+        self.email = email
+        self.is_logged_in = False
+        self.inbox = []
 
-    @staticmethod
-    def does_user_exist(db, user_name):
-        if DbManager.fetch(db, user_name):
+    @classmethod
+    def does_user_exist(cls, user_name):
+        if DbManager.fetch(user_name):
             return True
         else:
             return False
 
-    def log_in(self, db, user_name, password):
-        if self.does_user_exist(db, user_name):
-            if DbManager.fetch(db, user_name)[0]["password"] == password:
-                self.logged_in = True
+    @classmethod
+    def log_in(cls, user_name, password):
+        if cls.does_user_exist(user_name):
+            user_data = DbManager.fetch(user_name)[0]
+            if user_data["password"] == password:
+                user = cls(**user_data)
+                user.is_logged_in = True
                 return True
         else:
             return False
 
-    def log_out(self):
-        self.logged_in = False
+    @classmethod
+    def log_out(cls):
+        cls.is_logged_in = False
 
     @staticmethod
-    def add(db, user_name, password):
-        if not DbManager.fetch(db, user_name):
-            DbManager.add(db, {"username": user_name, "password": password, "role": "user", "inbox": {}})
+    def hash_password(password):
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    @classmethod
+    def add(cls, username, password, email, role="user"):
+        if not DbManager.fetch(username):
+            password_hash = cls.hash_password(password)
+            DbManager.add(
+                {"username": username, "password_hash": password_hash, "email": email, "role": role, "inbox": []})
             return True
         else:
             return False
 
-    @staticmethod
-    def remove(db, user_name):
-        if DbManager.fetch(db, user_name):
-            DbManager.remove(db, user_name)
+    @classmethod
+    def remove(cls, user_name):
+        if DbManager.fetch(user_name):
+            DbManager.remove(user_name)
             return True
         else:
             return False
 
-    @staticmethod
-    def get(db, username=None):
+    @classmethod
+    def get(cls, username=None):
         if username is None:
-            users = DbManager.fetch(db)
-        users = DbManager.fetch(db, username)
+            users = DbManager.fetch()
+        else:
+            users = DbManager.fetch(username)
         return users
+
+    def load_inbox(self):
+        pass
