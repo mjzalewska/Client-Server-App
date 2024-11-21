313e0cfe692857d1ebeb24501b96bf39224bde56
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-11-21 23:38:00 +0100

    Refactored the DbManager class methods to work with changed users.json format (json instead of json array); deleted unused update() method

--- db_manager.py
+++ db_manager.py
@@ -6,7 +6,7 @@ class DbManager:
     db_file = "users.json"
 
     @classmethod
-    def add(cls, data):
+    def save(cls, username, user_data):
         if os.path.exists(cls.db_file):
             with open(cls.db_file, 'r') as db:
                 try:
@@ -16,83 +16,59 @@ class DbManager:
         else:
             existing_data = []
 
-        existing_data.append(data)
+        existing_data[username] = user_data
 
         with open(cls.db_file, 'w') as db:
             json.dump(existing_data, db, indent=4)
 
     @classmethod
-    def update(cls, username, key, new_key_val):
+    def delete(cls, username):
         try:
             if os.path.exists(cls.db_file):
-                with open(cls.db_file, "r", encoding="utf-8") as old_db_file:
+                updated_data = {}
+                with open(cls.db_file, "r", encoding="utf-8") as current_db:
                     try:
-                        existing_data = json.load(old_db_file)
-                        updated_recs = 0
-                        for record in existing_data:
-                            if record["username"] == username:
-                                record[key] = new_key_val
-                                updated_recs += 1
+                        current_data = json.load(current_db)
+                        removed_items = 0
+                        for user, user_data in current_data.items():
+                            if user != username:
+                                updated_data[user] = user_data
                             else:
-                                pass
-                    except json.JSONDecodeError:
-                        existing_data = []
-            else:
-                raise FileNotFoundError
-        except FileNotFoundError:
-            return {"message": "database not found"}
-        except Exception as e:
-            return {"message": e}
-
-        with open(cls.db_file, 'w') as new_db_file:
-            json.dump(existing_data, new_db_file, indent=4)
-            return {"message": f"updated {updated_recs} record(s)"}
-
-    @classmethod
-    def remove(cls, user_name):
-        try:
-            if os.path.exists(cls.db_file):
-                updated_data = []
-                with open(cls.db_file, "r", encoding="utf-8") as old_db_file:
-                    try:
-                        existing_data = json.load(old_db_file)
-                        removed_recs = 0
-                        for record in existing_data:
-                            if record["username"] != user_name:
-                                updated_data.append(record)
-                            else:
-                                removed_recs += 1
+                                removed_items += 1
                     except json.JSONDecodeError:
                         updated_data = []
             else:
                 raise FileNotFoundError
         except FileNotFoundError:
-            return {"message": "database not found"}
+            return FileNotFoundError
         except Exception as e:
-            return {"message": e}
+            return e
 
-        with open(cls.db_file, 'w') as new_db_file:
-            json.dump(updated_data, new_db_file, indent=4)
-            return {"message": f"updated {removed_recs} record(s)"}
+        with open(cls.db_file, 'w') as new_db:
+            json.dump(updated_data, new_db, indent=4)
+            return {"message": f"Deleted {removed_items} record(s)"}
 
     @classmethod
-    def fetch(cls, username=None):
+    def get(cls, username=None):
         try:
             if os.path.exists(cls.db_file):
                 with open(cls.db_file, "r", encoding="utf-8") as db:
                     try:
                         users = json.load(db)
                         if username is None:
-                            return [record for record in users]
+                            return users
                         else:
-                            return [record for record in users if record.get("username") == username]
+                            return users[username]
                     except json.JSONDecodeError:
                         return []
                     except KeyError:
-                        return []
+                        return KeyError
             else:
                 raise FileNotFoundError
         except FileNotFoundError:
-            return [{"message": "database not found"}]
+            return FileNotFoundError
         except Exception as e:
-            return [{"message": e}]
+            return e
+
+
+print(DbManager.delete("user1"))
