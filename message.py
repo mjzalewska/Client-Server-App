90534ea4adedecc53816f3f003713ba368514d2b
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2025-01-06 22:03:06 +0100

    Added package folders and modules for the messanging functionality

---
+++ message.py
@@ -0,0 +1,16 @@
+from message_dao import MessageDAO
+
+
+class Message:
+    def __init__(self):
+        self.length = 255
+        self.limit = 5
+
+    def compose(self):
+        pass
+
+    def read(self):
+        pass
+
+    def delete(self):
+        pass
---
+++ message_dao.py
@@ -0,0 +1,15 @@
+class MessageDAO:
+    pass
+
+    def generate_hash(self):
+        pass
+
+    def save_message(self):
+        pass
+
+    def get_message(self):
+        pass
+
+    def delete_message(self):
+        pass
+
