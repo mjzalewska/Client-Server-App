debe113bc2bfd659979e46b15ecc40ec117ec05c
magdazalewska <m.jarzebska.zalewska@gmail.com>
2025-02-15 00:18:29 +0100

    Added display_email() method to the Display class

--- display.py
+++ display.py
@@ -3,6 +3,7 @@ from prettytable import PrettyTable
 
 
 class Display:
+
     @staticmethod
     def display_message(message):
         if message.get("message"):
@@ -16,14 +17,14 @@ class Display:
                         print(f"{key}: {value}")
                     print()
                 elif display_type == "tabular" and data_content:
-                    Display.display_tables(message)
+                    Display.display_table(message)
                     print()
             except (ValueError, TypeError):
                 logging.error(f"Error displaying message data")
                 print(f"Error displaying message")
 
     @staticmethod
-    def display_tables(message):
+    def display_table(message):
         if not message or not message.get("data"):
             print("No data to display")
             return
@@ -50,7 +51,7 @@ class Display:
         if not messages:
             print("No messages to display")
             return
-        inbox_columns = [" ", "SENDER", "SUBJECT", "DATE"]
+        inbox_columns = ["#", "SENDER", "SUBJECT", "DATE"]
         table = PrettyTable(field_names=inbox_columns)
         message_number = 1
         for message in messages.values():
@@ -65,3 +66,23 @@ class Display:
         table.max_width = 30
         table.hrules = True
         print(table)
+
+    @staticmethod
+    def display_email(email_num, message):
+        if not message or not message.get("data"):
+            print("No data to display")
+            return
+        inbox = message["data"][0]
+        if not inbox:
+            print("No messages to display")
+            return
+        messages = list(inbox.values())[0]
+        for field_name, field in messages[Display._map_email_id(email_num, messages)].items():
+            print(f"{field_name.title()}: {field}")
+
+    @staticmethod
+    def _map_email_id(email_num, messages_dict):
+        message_id_mapping = {num + 1: item for num, item in enumerate(list(messages_dict.keys()))}
+        for key in message_id_mapping.keys():
+            if key == email_num:
+                return message_id_mapping[key]
